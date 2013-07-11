12ecf1b9e8444afc5c7692de6941bf7f6fce96fb
Nikki Koole <nikkikoole@gmail.com>
2013-07-11 18:27:03 +0200

    mass, gravity, friction

--- CSSTransition/js/script.js
+++ CSSTransition/js/script.js
@@ -1,22 +1,21 @@
 window.onload = function() {
-
-    // this program simulates a bouncing ball.
-    // it will work with these variables:
+    // This program simulates a bouncing ball.
+    // It will work with these variables:
     // -Mass (the larger the mass, the less bouncing distance)
     // -Gravity (the larger the gravity, the higher the speed of movement)
     // -Friction (the larger the friction, the less bouncing distance (visually the same as Mass))
 
-
     var ball = document.getElementById('ball');
+    var massInput = document.getElementById('mass');
+    var frictionInput = document.getElementById('friction');
     var gravityInput = document.getElementById('gravity');
     var resetButton = document.getElementById('reset');
-    var gravity = gravityInput.value;    
 
-    var decay = 1.0;
-    var decayRate = 0.7;
-    var maxDistance = 400;
-    var currentDistance = 0;
-    
+    var maximumMass = 100; // At this Mass there will be no bounce. 
+    var decay = 1.0; // The rate at which values get multplied, when 0 there will be no bounce and no duration.
+    var startDistance = 400; // The initial height of the ball when dropped.
+    var currentDistance = 0; //The next distance the ball will travel.
+
     resetButton.onclick = function() {
         decay = 1.0;        
         ball.className = "up";
@@ -24,43 +23,47 @@ window.onload = function() {
         ball.style['-webkit-transform'] = 'translateY(0px)';    
     }
 
-    gravityInput.onchange = function() {
-        console.log("new Gravity: "+gravityInput.value);  
-        gravity = gravityInput.value;       
+    if (ball) {
+        ball.addEventListener("webkitTransitionEnd", updateTransition, true);
+        if (ball.className === '') {
+            ball.className = 'up';      
+        }
+        updateTransition();
     }
 
+    function calculateDecay()
+    {
+        decay *= Math.max(0, (1.0 -(massInput.value / maximumMass) -(frictionInput.value / 100.0 )));
+    } 
 
-      if (ball) {
-            ball.addEventListener("webkitTransitionEnd", updateTransition, true);
-            if (ball.className === '') {
-              ball.className = 'up';      
-            }
-            updateTransition();
-      }
+    function getBounceDistance(inDistance)
+    {
+        return decay*inDistance;
+    }
 
-     function updateTransition(  ) { 
-         if (ball.className === 'down'){
+    function getBounceDuration()
+    {
+        return (1.0 / (gravityInput.value/9.8));      
+    }
+
+    function updateTransition(  ) { 
+        if (ball.className === 'down'){
             // at this point the ball collides with the ground,
-            // thus this is where friction takes it's percentage and the bounce distance and speed
-            // will need to be calculated.
-            decayRate = (gravity/20.0);
-            decay *= decayRate;            
-            currentDistance = maxDistance * decay;
-            
+            // this is where the output force is calculated.
+            calculateDecay();
+            currentDistance = getBounceDistance(startDistance);
+
             ball.className = 'up';
-            ball.style['-webkit-transition-duration'] = (1 * decay)+'s';
-            ball.style['-webkit-transition-timing-function'] = 'cubic-bezier(0,0,0.20,1)'
+            ball.style['-webkit-transition-duration'] = (getBounceDuration() * decay)+'s';
+            ball.style['-webkit-transition-timing-function'] = 'cubic-bezier(0,0,0.20,1)';
             ball.style['-webkit-transform'] = 'translateY('+(400-currentDistance)+'px)';
-         }
-         else if (ball.className === 'up'){
-            // at this point the ball is at it's highest, 
-            // perhaps it's a good idea to save the distance to the ground ?            
+        }
+        else if (ball.className === 'up'){
             ball.className = 'down';
-            ball.style['-webkit-transition-duration'] = (1 * decay)+'s';
+            ball.style['-webkit-transition-duration'] = (getBounceDuration() * decay)+'s';
             ball.style['-webkit-transition-timing-function'] = 'cubic-bezier(0.80,0,1,1)';
             ball.style['-webkit-transform'] = 'translateY(400px)';
-          }
+        }
 
-     };
-
-}
+    };
+};
