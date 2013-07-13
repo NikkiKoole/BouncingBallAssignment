841ae341c89fcdba717c8bd19091083eee71da93
Nikki Koole <nikkikoole@gmail.com>
2013-07-13 17:56:10 +0200

    made simple native limejs dragger

--- LimeJS/balls/balls.js
+++ LimeJS/balls/balls.js
@@ -13,60 +13,41 @@ goog.require('lime.animation.FadeTo');
 goog.require('lime.animation.ScaleTo');
 goog.require('lime.animation.MoveTo');
 
+balls.WIDTH = 600;
+balls.HEIGHT = 480;
 
 // entrypoint
-balls.start = function(){
 
-	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
+balls.start = function () {
+    "use strict";
 
-	    target = new lime.Layer().setPosition(512,384),
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+    var director = new lime.Director(document.body, balls.WIDTH, balls.HEIGHT),
+        gamescene = new lime.Scene(),
+        layer = (new lime.Layer()).setPosition(30, 30),
+        dragger = (new lime.Sprite()).setSize(15, 30).setFill(0, 255, 255).setPosition(0, 10),
+        draggerLine = (new lime.Sprite()).setSize(150, 4).setFill(0, 255, 0).setPosition(75, 10);
+    layer.appendChild(dragger);
+    gamescene.appendChild(layer);
 
+    director.makeMobileWebAppCapable();
+    balls.inspect = new lime.Label().setPosition(200, 20).setText('');
 
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
+    goog.events.listen(dragger, ['mousedown', 'touchstart'], function (e) {
+        e.event.stopPropagation();
+        var drag = e.startDrag(false, new goog.math.Box(10, 150, 10, 0));
 
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
-
-	director.makeMobileWebAppCapable();
-
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
-
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
-
-        title.runAction(new lime.animation.FadeTo(1));
-
-        //let target follow the mouse/finger
-        e.startDrag();
-
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-
-            title.runAction(new lime.animation.FadeTo(0));
+        goog.events.listen(drag, lime.events.Drag.Event.MOVE, function () {
+            var pos = dragger.getPosition();
+            console.log(Math.round(pos.x) + ' ' + Math.round(pos.y));
         });
-
 
     });
 
-	// set current scene active
-	director.replaceScene(scene);
+
+    layer.appendChild(draggerLine);
+
+    // set active scene
+    director.replaceScene(gamescene);
 
 }
 
